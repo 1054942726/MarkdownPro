c57e048820c7d9cf07aaf38f121e1734b5059939
1054942726 <1054942726@qq.com>
2020-11-07 17:19:05 +0800

    end

---
+++ markdown-pro/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Pro />
+  </div>
+</template>
+
+<script>
+import Pro from "./components/Pro/Pro.vue";
+
+export default {
+  name: "App",
+  components: {
+    Pro
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
